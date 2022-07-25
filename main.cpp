b04d47d49869217c2175e14464ef02f142f63623
Apfelvater <>
2022-07-25 19:43:07 +0200

    Intitial Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Main.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    char c = 0;
+    while(c != 'q') {
+        cin >> c;
+        cout << c << endl;
+    }
+    return 0;
+}
