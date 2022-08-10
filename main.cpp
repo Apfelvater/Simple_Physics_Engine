3461cbf40566bfb219e2a268027856eceaa928e1
Apfelvater <>
2022-08-10 22:59:17 +0200

    first thread commit

--- main.cpp
+++ main.cpp
@@ -2,19 +2,28 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <thread>
+#include "Field.h"
 
 
 using namespace std;
 
 int main() {
-    char c = 0;
-    
-    draw_matrix();
-/*
-    while(true) {
-        update_matrix();
-    }
+
+
+    // Initialisation
+    Field field(25);
+    field.draw_field();
+
+    thread builder_thread(field_builder, field);
+    //start all the other threads
+    builder_thread.join();
+
     return 0;
-*/
 }
 
+void field_builder(Field field) {
+    while(true) {
+        
+    }
+}
