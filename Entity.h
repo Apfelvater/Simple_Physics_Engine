de8aba112c26dfc8923f5e92b1eab026ae311d7a
Nicholas <nicho1@Nicho-Debian.fritz.box>
2022-08-12 00:44:51 +0200

    Initial Entity.h commit

---
+++ Entity.h
@@ -0,0 +1,19 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <iostream>
+#include "Atom.h"
+
+class Entity: public Atom {
+
+    private:
+    int size_x;
+    int size_y;
+
+    public:
+    
+
+
+};
+
+#endif
