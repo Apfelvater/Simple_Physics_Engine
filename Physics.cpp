3507446aa3accf4dea4194de0c9029653e00e5af
Nicholas <nicho96@hotmail.de>
2022-08-22 23:21:20 +0200

    +Distance calculation functions

--- Physics.cpp
+++ Physics.cpp
@@ -11,3 +11,19 @@ float Physics::get_distance_by_a(float dtime, float acceleration) {
     // Get Distance traveled in dtime with constant acceleration
     return dtime * dtime * acceleration;
 }
+
+vector<float> Physics::get_distance_by_v(float dtime, vector<float> velocity) {
+    vector<float> distance(velocity.size());
+    for (int i = 0; i < velocity.size(); i++) {
+        distance[i] = velocity[i] * dtime;
+    }
+    return distance;
+}
+
+vector<float> Physics::get_distance_by_a(float dtime, vector<float> acceleration) {
+    vector<float> distance(acceleration.size());
+    for (int i = 0; i < acceleration.size(); i++) {
+        distance[i] = acceleration[i] * dtime * dtime;
+    }
+    return distance;
+}
--- Physics.h
+++ Physics.h
@@ -1,13 +1,17 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
 
+#include <vector>
+
 using namespace std;
 
 class Physics {
 
     static float get_distance_by_v(float dtime, float velocity);
-
     static float get_distance_by_a(float dtime, float acceleration);
+
+    static vector<float> get_distance_by_v(float dtime, vector<float> velocity);
+    static vector<float> get_distance_by_a(float dtime, vector<float> acceleration);
 
 };
 
